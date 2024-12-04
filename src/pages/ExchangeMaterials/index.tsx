0896f48bd50d6c7a8c9f390f4832aaf45da349d3
Kristyan Carvalho <kristyancarvalho@hotmail.com>
2024-12-04 01:09:27 -0300

    [FEAT] done exchange materials page.

--- src/pages/ExchangeMaterials/index.tsx
+++ src/pages/ExchangeMaterials/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import NavigationBar from '@/components/NavigationBar';
-import { Button, Typography } from '@mui/material';
+import { useState } from "react";
+import styled from "styled-components";
+import NavigationBar from "@/components/NavigationBar";
+import { Button, Typography } from "@mui/material";
 
 interface MaterialType {
   type: string;
@@ -11,45 +11,36 @@ interface MaterialType {
 
 function ExchangeMaterialsPage() {
   const [selectedMaterial, setSelectedMaterial] = useState<MaterialType | null>(null);
+  const [confirmedMaterial, setConfirmedMaterial] = useState<MaterialType | null>(null);
 
   const materialTypes: MaterialType[] = [
-    { 
-      type: 'Papel', 
-      color: '#FFD400',
-      icon: 'icons/exchange-icon.svg'
-    },
-    { 
-      type: 'Plástico', 
-      color: '#FF3C36',
-      icon: 'icons/exchange-icon.svg'
-    },
-    { 
-      type: 'Metal', 
-      color: '#1ADB72',
-      icon: 'icons/exchange-icon.svg'
-    },
-    { 
-      type: 'Vidro', 
-      color: '#0055FF',
-      icon: 'icons/exchange-icon.svg'
-    }
+    { type: "Papel", color: "#FFD400", icon: "icons/exchange-icon.svg" },
+    { type: "Plástico", color: "#FF3C36", icon: "icons/exchange-icon.svg" },
+    { type: "Metal", color: "#1ADB72", icon: "icons/exchange-icon.svg" },
+    { type: "Vidro", color: "#0055FF", icon: "icons/exchange-icon.svg" },
   ];
 
   const materialScores = [
-    { type: 'Papel', score: 200, weight: 20 },
-    { type: 'Plástico', score: 200, weight: 60 },
-    { type: 'Metal', score: 200, weight: 100 },
-    { type: 'Vidro', score: 40, weight: 22 },
+    { type: "Papel", score: 200, weight: 20 },
+    { type: "Plástico", score: 200, weight: 60 },
+    { type: "Metal", score: 200, weight: 100 },
+    { type: "Vidro", score: 40, weight: 22 },
   ];
 
   const handleMaterialSelect = (material: MaterialType) => {
-    if (selectedMaterial?.type === material.type) {
-      setSelectedMaterial(null);
-    } else {
-      setSelectedMaterial(material);
+    setSelectedMaterial(selectedMaterial?.type === material.type ? null : material);
+  };
+
+  const handleConfirm = () => {
+    if (selectedMaterial) {
+      setConfirmedMaterial(selectedMaterial);
     }
   };
-  
+
+  const handleBack = () => {
+    setSelectedMaterial(null);
+    setConfirmedMaterial(null);
+  };
 
   return (
     <>
@@ -57,50 +48,99 @@ function ExchangeMaterialsPage() {
       <Container>
         <InfoHeader>
           <img src="icons/exchange-page-icon.svg" alt="Exchange Page Icon" />
-          <div id='header-text'>
-            <InfoHeaderTitle variant="h1">
-              Trocar Materiais
-            </InfoHeaderTitle>
+          <div id="header-text">
+            <InfoHeaderTitle variant="h1">Trocar Materiais</InfoHeaderTitle>
             <InfoHeaderSubtitle variant="h4">
               Recicle itens para ganhar pontos!!
             </InfoHeaderSubtitle>
           </div>
         </InfoHeader>
 
-        <Card>
-          <CardTitle variant='h3'>
-            Coloque o item na balança, e selecione o tipo de material.
-          </CardTitle>
-          <CardButtons>
-            {materialTypes.map((material) => (
-              <CardButton
-                key={material.type}
-                style={{ 
-                  backgroundColor: material.color,
-                  opacity: selectedMaterial && selectedMaterial.type !== material.type ? 0.5 : 1
-                }}
-                disabled={selectedMaterial !== null && selectedMaterial.type !== material.type}
-                onClick={() => handleMaterialSelect(material)}
-              >
-                <img src={material.icon} alt={`${material.type} icon`} />
-                <CardButtonTitle>
-                  {material.type}
-                </CardButtonTitle>
-              </CardButton>
-            ))}
-          </CardButtons>
-        </Card> 
+        {!confirmedMaterial ? (
+          <>
+            <Card>
+              <CardTitle variant="h3">
+                Coloque o item na balança, e selecione o tipo de material.
+              </CardTitle>
+              <CardButtons>
+                {materialTypes.map((material) => (
+                  <CardButton
+                    key={material.type}
+                    style={{
+                      backgroundColor: material.color,
+                      opacity: selectedMaterial && selectedMaterial.type !== material.type ? 0.5 : 1,
+                    }}
+                    disabled={selectedMaterial !== null && selectedMaterial.type !== material.type}
+                    onClick={() => handleMaterialSelect(material)}
+                  >
+                    <img src={material.icon} alt={`${material.type} icon`} />
+                    <CardButtonTitle>{material.type}</CardButtonTitle>
+                  </CardButton>
+                ))}
+              </CardButtons>
+            </Card>
 
-        <ConfirmButton 
-          disabled={selectedMaterial === null}
-          style={{ 
-            backgroundColor: selectedMaterial ? '#18DBB1' : '#ACACAC' 
-          }}
-        >
-          <ConfirmButtonTitle>
-            Confirmar
-          </ConfirmButtonTitle>
-        </ConfirmButton>
+            <ConfirmButton
+              disabled={selectedMaterial === null}
+              onClick={handleConfirm}
+              style={{
+                backgroundColor: selectedMaterial ? "#18DBB1" : "#ACACAC",
+              }}
+            >
+              <ConfirmButtonTitle>Confirmar</ConfirmButtonTitle>
+            </ConfirmButton>
+          </>
+        ) : (
+          <>
+            <ConfirmationCard>
+              <ConfirmationTitle variant="h3">
+                Você selecionou:
+              </ConfirmationTitle>
+              <ConfirmationHeader style={{ backgroundColor: confirmedMaterial.color }}>
+                {confirmedMaterial.type}
+              </ConfirmationHeader>
+              <ConfirmationBody>
+                <ConfirmationInfo>
+                  <ConfirmationTitle variant="h4">
+                    Peso do material
+                  </ConfirmationTitle>
+
+                  <ConfirmationMainInfo>
+                    {
+                      materialScores.find((m) => m.type === confirmedMaterial.type)?.weight
+                    }
+                  </ConfirmationMainInfo>
+                </ConfirmationInfo>
+
+                <ConfirmationInfo>
+                  <ConfirmationTitle variant="h4">
+                    Pontos a adquirir:
+                  </ConfirmationTitle>
+
+                  <ConfirmationMainInfo>
+                    <img src="icons/points-icon.svg" alt="" />
+                    {
+                      materialScores.find((m) => m.type === confirmedMaterial.type)?.score
+                    }
+                  </ConfirmationMainInfo>
+                </ConfirmationInfo>
+
+              </ConfirmationBody>
+            </ConfirmationCard>
+            <ConfirmationActions>
+              <BackButton onClick={handleBack}>
+                <ConfirmButtonTitle>
+                  Voltar
+                </ConfirmButtonTitle>
+              </BackButton>
+              <EarnPointsButton>
+                <ConfirmButtonTitle>
+                  Adquirir Pontos
+                </ConfirmButtonTitle>
+              </EarnPointsButton>
+            </ConfirmationActions>
+          </>
+        )}
 
         <ScoreCards>
           {materialScores.map(({ type, score, weight }, index) => (
@@ -120,27 +160,19 @@ function ExchangeMaterialsPage() {
   );
 }
 
-
-/*
-#FFD400
-#FF3C36
-#1ADB72
-#05F
-*/
-
 const Container = styled.div`
   padding: 64px 32px;
   height: 100vh;
   width: 100vw;
-  background-color: #F3F2F2;
+  background-color: #f3f2f2;
 `;
 
 const InfoHeader = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
-  font-family: 'Poppins', sans-serif; 
-  
+  font-family: "Poppins", sans-serif;
+
   && img {
     width: 150px;
     margin-bottom: -32px;
@@ -149,30 +181,30 @@ const InfoHeader = styled.div`
 
 const InfoHeaderTitle = styled(Typography)`
   font-weight: bold;
-`
+`;
 
 const InfoHeaderSubtitle = styled(Typography)`
-  color: #ACACAC;
-  font-family: 'Poppins', sans-serif;
-`
+  color: #acacac;
+  font-family: "Poppins", sans-serif;
+`;
 
 const Card = styled.div`
   margin-top: 64px;
   border-radius: 16px;
   width: 100%;
   padding: 32px;
-  background: #FFFFFF;
+  background: #ffffff;
   text-align: center;
-`
+`;
 
-const CardTitle = styled(Typography)``
+const CardTitle = styled(Typography)``;
 
 const CardButtons = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 64px;
   margin-top: 64px;
-`
+`;
 
 const CardButton = styled(Button)`
   && {
@@ -191,31 +223,111 @@ const CardButton = styled(Button)`
       opacity: 0.5;
     }
   }
-`
-
+`;
 
 const CardButtonTitle = styled.span`
   font-size: 2rem;
-`
+`;
 
 const ConfirmButton = styled(Button)`
-    && {
-      margin-top: 64px;
-      width: 100%;
-      height: 75px;
-      background-color: #18DBB1;
-      border-radius: 16px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 16px;
-      color: white;
-    }
-`
+  && {
+    margin-top: 64px;
+    width: 100%;
+    height: 75px;
+    background-color: #18dbb1;
+    border-radius: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    color: white;
+  }
+`;
 
 const ConfirmButtonTitle = styled.span`
   font-size: 2rem;
+`;
+
+const ConfirmationCard = styled.div`
+  margin-top: 64px;
+  padding: 32px;
+  background: #ffffff;
+  border-radius: 16px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ConfirmationHeader = styled.div`
+  width: 60%;
+  text-align: center;
+  padding: 16px;
+  color: #ffffff;
+  font-size: 1.8rem;
+  border-radius: 8px;
+  font-family: 'Poppins', sans-serif;
+`;
+
+const ConfirmationTitle = styled(Typography)``
+
+const ConfirmationBody = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  `;
+
+const ConfirmationInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 48px;
 `
+const ConfirmationMainInfo = styled.span`
+  font-family: 'Poppins', sans-serif;
+  font-size: 6rem;
+  font-weight: 500;
+
+  margin-top: 32px;
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`
+
+const ConfirmationActions = styled.div`
+  margin-top: 64px;
+  display: flex;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+const BackButton = styled(Button)`
+  && {
+    width: 100%;
+    height: 75px;
+    background-color: #ACACAC;
+    border-radius: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    color: white;
+  }
+`;
+
+const EarnPointsButton = styled(Button)`
+  && {
+    width: 100%;
+    height: 75px;
+    background-color: #18dbb1;
+    border-radius: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    color: white;
+  }
+`;
 
 const ScoreCards = styled.div`
   display: flex;
@@ -266,6 +378,5 @@ const RightSection = styled.div`
     color: #888;
   }
 `;
-
 
 export default ExchangeMaterialsPage;
