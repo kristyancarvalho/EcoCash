4e2aad49e46de96ff40b877b0f02f2438abcce7f
Kristyan Carvalho <kristyancarvalho@hotmail.com>
2024-12-07 17:53:39 -0300

    [FIX] variable declaration by it´s state changing.

--- src/api/arduino.tsx
+++ src/api/arduino.tsx
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-let ip = "192.168.97.225";
+const ip = "192.168.97.225";
 
 export async function getPeso() {
   try {
-    let peso = await axios.get(`http://${ip}/peso`);
+    const peso = await axios.get(`http://${ip}/peso`);
     return Number(peso.data);
   }
   catch (e) {
